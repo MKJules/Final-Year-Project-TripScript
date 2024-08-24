55ec6af8e3cf309df707cf7de5d906bf162e468d
MKJules <kwasikonadumaafo@gmail.com>
2024-08-24 15:00:29 +0000

    firebase authentication
    added the firebase authentication to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:trip_script/details_screen.dart';
 import 'package:trip_script/home1.dart';
 import 'package:trip_script/onboarding1.dart';
+
+import 'firebase_options.dart';
 // import 'package:trip_script/signup.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       ScreenUtilInit(
         designSize: const Size(430,932),
