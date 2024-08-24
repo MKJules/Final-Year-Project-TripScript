700c1a36011a8bb8e3f657d99593ecb7592ada06
Sena <emmanueldokeii@gmail.com>
2024-08-24 07:44:15 +0000

    completed profile, wishlist and notification screens

--- lib/wishlist.dart
+++ lib/wishlist.dart
@@ -1,10 +1,121 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Wishlist extends StatelessWidget {
   const Wishlist({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List collection = [];
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.w),
+              ),
+              SvgPicture.asset(
+                'icons/back.svg',
+                color: Colors.white,
+                width: 24.w,
+                height: 24.h,
+              ),
+              SizedBox(width: 15.w),
+              Text(
+                'Back',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.h),
+          Container(
+            width: double.infinity,
+            height: 750.h,
+            padding: EdgeInsets.symmetric(
+              horizontal: 30.w,
+              vertical: 42.h,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50.r),
+                topRight: Radius.circular(50.r),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Wishlist',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.sp,
+                  ),
+                ),
+                SizedBox(height: 20.h),
+                SizedBox(
+                  height: 600.h,
+                  child: GridView.builder(
+                    padding: EdgeInsets.zero,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 20.w,
+                      mainAxisSpacing: 20.h,
+                    ),
+                    itemCount: collection.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return DottedBorder(
+                          color: const Color.fromRGBO(125, 125, 125, 1),
+                          strokeWidth: 2,
+                          dashPattern: const [6, 3],
+                          borderType: BorderType.RRect,
+                          radius: Radius.circular(20.r),
+                          child: SizedBox(
+                            width: 175.w,
+                            height: 175.h,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.add,
+                                  size: 35.sp,
+                                ),
+                                Text(
+                                  'New Collection Wishlist',
+                                  style: TextStyle(
+                                    fontSize: 10.sp,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      } else {
+                        return const SizedBox();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
